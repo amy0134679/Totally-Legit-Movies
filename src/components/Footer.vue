f03cb5dfa3522f8da5ece7c28478887508439967
Amy <amy0134679@gmail.com>
2023-06-01 14:54:35 -0400

    Styled login page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,8 +13,13 @@
 
 <style scoped>
 .footer-container {
+  padding-inline-start: 20px;
   background-color: pink;
   width: 75vw;
   height: 25vh;
+}
+
+h4 {
+font-size: 20px
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -17,17 +17,73 @@ const login = () => {
 </script>
 
 <template>
+   <img
+        id="background"
+        :src="`https://images8.alphacoders.com/749/749455.png`"
+        alt=""
+      />
+
   <form class="login-container" @submit.prevent="login()">
+<h2>Sign In</h2>
     <input type="text" placeholder="username" v-model="username" />
     <input type="password" placeholder="password" v-model="password" />
-    <input type="submit" value="Login" />
+    <input type="submit" id = "button" value="Login" />
   </form>
 </template>
 
 <style>
+
+#background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+}
+
 .login-container {
-  width: 50%;
+  border-radius: 20px;
+  margin: auto;
+  padding: 40px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  color: white;
+  align-items: center;
+  background-color: rgba(252, 210, 235, 0.25);
+  filter: drop-shadow(-10px 10px 20px #827397);
+  margin-top: 200px;
+  height: 400px;
+  width: 30%;
   display: flex;
   flex-direction: column;
 }
+
+input {
+  padding: 15px;
+  margin: 10px;
+  border-color: rgba(0, 0, 0, 0);
+  border-radius: 5px;
+
+}
+
+#button {
+  
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin-right: 10px;
+  border-radius: 5px;
+  background-color: #d8b9c3;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+  filter: drop-shadow(-10px 10px 20px #827397);
+}
+
+#button:hover {
+  background-color: #dba4b5;
+  color: white;
+  filter: drop-shadow(-10px 10px 10px #827397);
+}
+
 </style>
+
+// change submit into button
