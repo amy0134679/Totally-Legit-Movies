e0db6f191f9b668434d050fdd8856672df7ac185
Amy <amy0134679@gmail.com>
2023-06-03 10:34:00 -0400

    Fixed button position

--- src/views/PurchaseView.vue
+++ src/views/PurchaseView.vue
@@ -14,17 +14,17 @@ const toggleModal = (id) => {
   selectedRecordId.value = id;
 };
 </script>
-
 <template>
-   <img
-    id="background"
-    :src="`https://img.freepik.com/free-vector/blue-pink-halftone-background_53876-99557.jpg?w=1480&t=st=1685754405~exp=1685755005~hmac=dabe7a36b79a0fa6348b82fb1eb6df8a5bef41a98be8992cf0cf7871f91b849f`"
-    alt=""
-  />
-
   <div>
-  <h1>Browse Trending Movies</h1>
-    <button @click="router.push('/cart')">Cart</button>
+    <img
+      id="background"
+      :src="`https://img.freepik.com/free-vector/blue-pink-halftone-background_53876-99557.jpg?w=1480&t=st=1685754405~exp=1685755005~hmac=dabe7a36b79a0fa6348b82fb1eb6df8a5bef41a98be8992cf0cf7871f91b849f`"
+      alt=""
+    />
+    <div class="header-container">
+      <h1>Browse Trending Movies</h1>
+      <button @click="router.push('/cart')">CART</button>
+    </div>
     <div v-if="store.movies" class="tiles">
       <div v-for="movie in store.movies" class="tile">
         <img
@@ -38,28 +38,69 @@ const toggleModal = (id) => {
 </template>
 
 <style scoped>
+.header-container {
+  padding-bottom: 20px;
+  border-bottom: 3px solid #dba4b5;
+  margin-top: 5px;
+  width: 95%;
+}
+
+button:hover {
+  background-color: #dba4b5;
+  color: white;
+  filter: drop-shadow(-10px 10px 10px #827397);
+}
+
 button {
   font-family: Verdana, Geneva, Tahoma, sans-serif;
-  margin-right: 10px;
   padding: 20px;
-  border-radius: 5px;
+  border-radius: 10px;
   border-color: #d8b9c3;
   background-color: #d8b9c3;
   color: white;
   font-size: 20px;
+  width: 150px;
+  height: 70px;
   cursor: pointer;
   filter: drop-shadow(-10px 10px 20px #827397);
+}
+
+.button-container button:hover {
+  background-color: #dba4b5;
+  color: white;
+  filter: drop-shadow(-10px 10px 10px #827397);
 }
 
 .tiles {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  row-gap: 40px;
+  margin-left: 30px;
 }
 
 img {
   width: 250px;
-  padding-bottom: 30px;
 }
+
+img:hover {
+  background-color: #e6e690;
+  color: white;
+  filter: drop-shadow(5px 10px 50px #d4bc6d);
+}
+
+.header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 30px;
+}
+
+h1 {
+  color: aliceblue;
+  font-family: "Chivo", sans-serif;
+  font-size: 50px;
+}
+
 #background {
   position: fixed;
   top: 0;
@@ -68,11 +109,4 @@ img {
   height: 100vh;
   z-index: -1;
 }
- h1 {
-  margin: 30px;
-  color:aliceblue;
-  font-family: 'Chivo', sans-serif;
-  font-size: 50px;
- }
-
 </style>
