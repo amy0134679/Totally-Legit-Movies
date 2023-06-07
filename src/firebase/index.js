2ef085d417afd564f74425f5bb2299603f9f3da8
Amy <amy0134679@gmail.com>
2023-06-07 19:55:48 -0400

    Added firebase files

---
+++ src/firebase/index.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1K27SQO3mRqd_XGByuPtpMhZrvTtDQ_I",
+  authDomain: "summative-8a23d.firebaseapp.com",
+  projectId: "summative-8a23d",
+  storageBucket: "summative-8a23d.appspot.com",
+  messagingSenderId: "462845908361",
+  appId: "1:462845908361:web:11c0537fb0a5cdf119fd58",
+  measurementId: "G-6T6FGS84CH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
